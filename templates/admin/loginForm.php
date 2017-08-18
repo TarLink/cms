1f835d500f149c6dade579c65c64c5fa9f24ea2d
Alexandru Bucur <aleks.bucur27@gmail.com>
2017-08-18 08:45:03 +0000

    Correct login form bug
    The labels overlap the input fields on some screen sizes

--- templates/admin/loginForm.php
+++ templates/admin/loginForm.php
@@ -12,16 +12,16 @@
 		
 		<div class="form-group">
 	
-			<label class="input-label col-sm-2" for="username">Username</label>
-			<div class="col-sm-10">
+			<label class="input-label col-sm-3 col-md-2" for="username">Username</label>
+			<div class="col-sm-9 col-md-10">
 				<input type="text"  name="username" class="form-control " id="username" placeholder="Your admin username" required autofocus maxlength="20" />
 			</div>
 		
 		</div>
 		<div class="form-group">
 		
-			<label class="input-label col-sm-2" for="password">Password</label>
-			<div class="col-sm-10">
+			<label class="input-label col-sm-3 col-md-2" for="password">Password</label>
+			<div class="col-sm-9 col-md-10">
 				<input type="password"   name="password" class="form-control " id="password" placeholder="Your admin password" required maxlength="20" />
 			</div>
 	
